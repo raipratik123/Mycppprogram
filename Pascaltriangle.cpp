04bd4d9ec7233452f6e706e6967c7d952828b6af
Pratik rai <raipratik788@gmail.com>
2026-02-06 21:27:26 +0530

    Added Pascal triangle program in C++

--- Pascaltriangle.cpp
+++ Pascaltriangle.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
     int n;
     cin>>n ;
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<=n; i++){
         int val=1;
-        for(int j=1; j<=i; j++){
+        for(int j=0; j<=i; j++){
                 cout<<val<<" ";
-                 val=val*((i-j)/(j+1));
+                 val=val*(i-j)/(j+1);
             }    
               cout<<endl;
         }
