bcea18b6e91f2301e198eae0eeb553c9a9314c04
Pratik rai <raipratik788@gmail.com>
2026-02-10 22:58:02 +0530

    Position

---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={12,30,40,50,60};
+   bool found = false;
+   int target;
+   cin>>target;
+for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+    if(arr[i]==target){
+        cout << i+1;
+        found = true;
+        break;
+    }
+}
+
+if(!found){
+    cout << "not found";
+}
+
+    
+    return 0;
+}
